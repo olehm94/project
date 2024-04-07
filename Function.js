124f44b020265697350a2af3611c9f11bcc8e1a3
Oleh <example@gmail.com>
2024-04-07 23:45:53 +0300

    FunctionsMethodsCycles

---
+++ Function.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage (text) {
+    console.log(text);
+    console.log(num);
+}
+
+
+showFirstMessage("Hello world");
+console.log(num);
+
+
+// function calc(a, b) {
+//     return (a+b);
+// }
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+
+    return num;
+}
+
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("hello");
+};
+
+logger();
+
+
+const calc = (a, b) => {
+    console.log();
+    return a=b;
+};
---
+++ MethodsOptions.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const str = "teSt";
+const arr = [1, 2,4];
+
+// console.log(str[2]);
+
+// console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+console.log(str);
+
+const fruit = "some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
